427fa4280116a87de4371893d816e923bb85d5d1
Vaishnav-192 <76587671+vaishnav-192@users.noreply.github.com>
2023-02-25 21:18:58 +0530

    validating

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const block = require('./chain');
+const cH = require('./cryptoHash');
 
 class blockchain{
     constructor(){
@@ -10,13 +11,27 @@ class blockchain{
         this.chain.push(newblock);
     }
 
-    // static isvalid(chain){
-    //     if(chain[0]!==block.gene())return false;
-    // }
+    static isvalid(chain){
+        
+        if(JSON.stringify(chain[0])!==JSON.stringify(block.gene()))return false;
+
+        for(let i=1; i<chain.length; i++){
+            const {timestam, prevhash, roothash, data}=chain[i];
+            const realLastHash = chain[i-1].roothash;
+
+            if(prevhash!==realLastHash)return false;
+            const validatedhash = cH(timestam, prevhash, roothash, data);
+            if(roothash!==validatedhash)return false;
+        }
+        return true;
+    }
+    
 }
 
 const blch = new blockchain();
 blch.addblock({data: "b1"});
 console.log(blch);
+const res  = blockchain.isvalid(blockchain.chain);
+console.log(res);
 
 module.exports = blockchain;
