463b0923523faba44c42a23f3b0258fb903a474c
Vaishnav-192 <76587671+vaishnav-192@users.noreply.github.com>
2023-04-01 15:37:35 +0530

    sucessfully information coming on localhost:3000 using express

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const blockch = require('./blockchain');
 const express = require('express');
+const blockchain = require('./blockchain');
 
 const app = express();
-
-app.get('/api/blocks', (req, res)=>{
+const blockch = new blockchain();
+app.get("/api/blocks", (req, res) => {
     res.json(blockch.chain);
-})
-
+});
 const PORT = 3000;
-app.listen(PORT, ()=>{`listening to ${PORT}`});
+app.listen(PORT, () => {
+    console.log(`listening to port: ${PORT}`)
+});
