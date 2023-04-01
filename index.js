5bdcaf405f4655885fb910b32894c4d08ee5f9ee
Vaishnav-192 <76587671+vaishnav-192@users.noreply.github.com>
2023-04-01 20:25:24 +0530

    added new function to write data into blockchain using postman

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const blockchain = require('./blockchain');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 const blockch = new blockchain();
 app.get("/api/blocks", (req, res) => {
     res.json(blockch.chain);
 });
+
+app.post("/api/mine", (req, res) => {
+    const { data } = req.body;
+
+    blockch.addblock({ data });
+    res.redirect("/api/blocks");
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`listening to port: ${PORT}`)
