f4cf5be50d8edf07fe447c04dbfcd106deb2914b
Vaishnav-192 <76587671+vaishnav-192@users.noreply.github.com>
2023-04-01 14:16:36 +0530

    changed hex to binary in cryptohash to solve the 'not inc dificulty and time to mine thr block'

--- cryptoHash.js
+++ cryptoHash.js
@@ -1,9 +1,10 @@
 const crypto = require('crypto');
+const hexToBinary = require('hex-to-binary');
 
 const cH = (...inputs)=>{
-    const hash = crypto.createHash('sha256')
-    hash.update(inputs.sort().join(''))
-    return hash.digest('hex')
+    const hash = crypto.createHash('sha256');
+    hash.update(inputs.sort().join(''));
+    return hexToBinary(hash.digest('hex'));
 }
 // result = cH("hello", "world");
 // console.log(result);
