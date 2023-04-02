4732c6152d6aa9840343c562bc91f2199b055869
Vaishnav-192 <76587671+vaishnav-192@users.noreply.github.com>
2023-04-02 15:03:28 +0530

    successfully implemented syncing across nodes. if a peer wants to add block: in postman change the port to peer port

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const blockch = require('./blockchain');
 const bodyParser = require('body-parser');
 const PubSub = require('./publishSubscribe');
+const request = require('request');
 
 const app = express();
 app.use(bodyParser.json());
 const blockchain = new blockch();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(
     () => pubsub.broadcastChain(), 1000
@@ -27,14 +31,24 @@ app.post("/api/mine", (req, res) => {
     //res.redirect("/api/mine");
     pubsub.broadcastChain();
     res.redirect("/api/blocks");
-})
+});
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+            console.log('replace chain on sync with', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    })
+};
+
 let PEER_PORT;
 if (process.env.GENERATE_PEER_PORT === 'true') {
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
-    console.log(`listening to port: ${PORT}`)
+    console.log(`listening to port: ${PORT}`);
+    syncChains();
 });
