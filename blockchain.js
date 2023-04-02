304d004c4f9100ff1938e98d50cb3391061914fa
Vaishnav-192 <76587671+vaishnav-192@users.noreply.github.com>
2023-04-02 14:10:30 +0530

    solved error: not able to compare in replaceChain

--- blockchain.js
+++ blockchain.js
@@ -1,60 +1,60 @@
 const block = require('./chain');
 const cH = require('./cryptoHash');
 
-class blockchain{
-    constructor(){
+class blockchain {
+    constructor() {
         this.chain = [block.gene()];
     }
 
-    addblock({data}){
-        const newblock = block.mineblock({prevBlock:this.chain[this.chain.length-1], data:data});
+    addblock({ data }) {
+        const newblock = block.mineblock({ prevBlock: this.chain[this.chain.length - 1], data: data });
         this.chain.push(newblock);
     }
 
-    replaceChain(chain){
-        if(chain<=this.chain.length){
+    replaceChain(chain) {
+        if (chain.length <= this.chain.length) {
             console.error("The incoming chain is not longer");
             return;
         }
-        if(!blockchain.isvalid(blch.chain)){
+        if (!blockchain.isvalid(blch.chain)) {
             console.error("The incoming chain is not valid");
             return;
         }
         this.chain = chain;
     }
 
-    static isvalid(chain){
+    static isvalid(chain) {
         // console.log(chain[0]);
         // console.log(block.gene());
-        if(JSON.stringify(chain[0])!==JSON.stringify(block.gene()))return false;
+        if (JSON.stringify(chain[0]) !== JSON.stringify(block.gene())) return false;
 
-        for(let i=1; i<chain.length; i++){
-            const {timestam, prevhash, data, roothash, difficulty, nonce}=chain[i];
+        for (let i = 1; i < chain.length; i++) {
+            const { timestam, prevhash, data, roothash, difficulty, nonce } = chain[i];
 
-            const lastDifficulty = chain[i-1].difficulty;
+            const lastDifficulty = chain[i - 1].difficulty;
 
-            const realLastHash = chain[i-1].roothash;
+            const realLastHash = chain[i - 1].roothash;
             //console.log(chain[i]);
 
-            if(prevhash!==realLastHash)return false;
+            if (prevhash !== realLastHash) return false;
             const validatedhash = cH(timestam, prevhash, data, difficulty, nonce);
-            if(roothash!==validatedhash)return false;
-            if(Math.abs(lastDifficulty-difficulty)>1)return false;
+            if (roothash !== validatedhash) return false;
+            if (Math.abs(lastDifficulty - difficulty) > 1) return false;
         }
         return true;
     }
-    
+
 }
 
 const blch = new blockchain();
 // blch.addblock({data: "b1"});
-for(let i=1; i<=5; i++){
+for (let i = 1; i <= 5; i++) {
     let s = "block";
-    s+= '0'+i;
-    blch.addblock({data: s});
+    s += '0' + i;
+    blch.addblock({ data: s });
 }
 console.log(blch.chain);
-const res  = blockchain.isvalid(blch.chain);
+const res = blockchain.isvalid(blch.chain);
 console.log(res);
 //console.log(block.gene())
 
